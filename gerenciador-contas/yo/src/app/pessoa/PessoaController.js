c2ebbf34b83c7c187afbeb33bfcd1149950052dd
Pedro Lira <pedrohsoares.lira@gmail.com>
2016-02-11 17:21:47 -0200

    Alterações

--- gerenciador-contas/yo/src/app/pessoa/PessoaController.js
+++ gerenciador-contas/yo/src/app/pessoa/PessoaController.js
@@ -1,15 +1,20 @@
 angular.module('gcontas').controller('PessoaController', function($scope, $http, $routeParams) {
 
 		$scope.pessoas =     [{"id":1, "firstName":"John", "lastName":"Doe"}, {"id":2, "firstName":"Anna", "lastName":"Smith"}, {"id":3 ,"firstName":"Peter", "lastName":"Jones"}];
+		$scope.titulo = "";
+		$scope.porcentagem = 0;
 
 		var idGet = $routeParams.pessoaId;
 
 		if(idGet) {
 			$scope.pessoa = {"id":1, "firstName":"John", "lastName":"Doe"};
-		} 
+			$scope.titulo = "Editando Registro Pessoa";			
+		} else {
+			$scope.titulo = "Cadastrar Novo Registro Pessoa";
+		}
 
 		$scope.submeter = function() {
-			alert("submeter");
+			$scope.pessoa = {};
 		}
 
 });
